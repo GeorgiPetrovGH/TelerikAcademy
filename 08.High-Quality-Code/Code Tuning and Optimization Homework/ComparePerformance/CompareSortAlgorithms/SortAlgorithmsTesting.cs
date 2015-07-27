76ef444a7dec5ae3bd08cf7f8d836fe8a1eca3f6
GeorgiPetrovGH <vortep.g@gmail.com>
2015-07-27 20:42:58 +0300

    HQC -> Code Tuning and Optimization Homework

---
+++ 08.High-Quality-Code/Code Tuning and Optimization Homework/ComparePerformance/CompareSortAlgorithms/SortAlgorithmsTesting.cs
@@ -0,0 +1,18 @@
+﻿namespace CompareSortAlgorithms
+{
+    using System;
+
+    public class SortAlgorithmsTesting
+    {
+        public static void Main()
+        {
+            string separator = new string('=', 60);
+
+            SortingAlgorithmsPerformanceTester.TestIntSortingPerformance();
+            Console.WriteLine(separator);
+            SortingAlgorithmsPerformanceTester.TestDoubleSortingPerformance();
+            Console.WriteLine(separator);
+            SortingAlgorithmsPerformanceTester.TestStringSortingPerformance();
+        }
+    }
+}
