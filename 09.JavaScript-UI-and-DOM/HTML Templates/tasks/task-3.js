c269ff6fecf414dd7624c13eb6582c73c528910f
GeorgiPetrovGH <vortep.g@gmail.com>
2015-08-04 23:05:23 +0300

    JS UI&DOM -> HTML Templates Homework

---
+++ 09.JavaScript-UI-and-DOM/HTML Templates/tasks/task-3.js
@@ -0,0 +1,18 @@
+function solve(){
+  return function(){
+    $.fn.listview = function(data){
+    	var $this = $(this),
+            templateSelector = '#' + $this.attr('data-template'),
+            templateHtml = $(templateSelector).html(),
+            template = handlebars.compile(templateHtml),
+            ind, len;
+
+        for(ind = 0, len = data.length; ind < len; ind += 1) {
+            $this.append(template(data[ind]));
+        }
+      
+    };
+  };
+}
+
+module.exports = solve;
