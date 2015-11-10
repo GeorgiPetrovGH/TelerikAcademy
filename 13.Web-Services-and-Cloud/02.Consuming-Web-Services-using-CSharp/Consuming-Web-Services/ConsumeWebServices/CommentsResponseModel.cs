3a303ab858021dbb43a09b8c7dece2593e831736
Georgi Petrov <vortep.g@gmail.com>
2015-11-10 17:39:40 +0200

    Consuming-Web-Services Homework

---
+++ 13.Web-Services-and-Cloud/02.Consuming-Web-Services-using-CSharp/Consuming-Web-Services/ConsumeWebServices/CommentsResponseModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsumeWebServices
+{
+    public class CommentsResponseModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Body { get; set; }
+    }
+}
---
+++ 13.Web-Services-and-Cloud/02.Consuming-Web-Services-using-CSharp/Consuming-Web-Services/ConsumeWebServices/Startup.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsumeWebServices
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+
+    using Newtonsoft.Json;    
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://jsonplaceholder.typicode.com");
+
+            Console.Write("Enter query string: (default value is odio)");
+            string queryString = Console.ReadLine();
+            if (string.IsNullOrEmpty(queryString))
+            {
+                queryString = "odio";
+            }
+
+            Console.Write("Enter number of items: (default value is 10) ");
+            int numberOfItems;
+            bool validNumber = int.TryParse(Console.ReadLine(), out numberOfItems);
+            if (!validNumber)
+            {
+                numberOfItems = 10;
+            }
+
+            PrintComments(httpClient, numberOfItems, queryString);
+            Console.WriteLine(new string('-', 45));
+            Console.WriteLine("Getting your result...");
+            Console.WriteLine(new string('-', 45));
+            Console.ReadLine();            
+        }
+
+        private static async void PrintComments(HttpClient httpClient, int numberOfItems, string query)
+        {
+            var response = await httpClient.GetAsync("comments");
+
+            var text = response.Content.ReadAsStringAsync().Result;
+            var jsons = JsonConvert.DeserializeObject<List<CommentsResponseModel>>(text);
+            var filtered = jsons.Where(x => x.Name.Contains(query) || x.Body.Contains(query)).Take(numberOfItems);
+
+            foreach (var comment in filtered)
+            {
+                Console.WriteLine("Name: {0}\nBody: {1}\nID: {2}", comment.Name, comment.Body, comment.Id);
+                Console.WriteLine(new string('-', 45));
+            }
+
+            Console.WriteLine("To exit press ENTER:");
+        }
+    }
+}
